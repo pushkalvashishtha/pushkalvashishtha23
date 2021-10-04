4166e78d069ff519964d7ccb7dcf3a15c6228caf
pushkalvashishtha <88961591+pushkalvashishtha@users.noreply.github.com>
2021-10-04 22:57:43 +0530

    twitter counts

---
+++ COUNT.js
@@ -0,0 +1,3 @@
+var tweet = prompt ('Compose Your Tweet');
+var tweetCount = tweet.length;
+alert('You have written ' +  tweetCount + ' characters, you have ' + (280 - tweetCount) + 'characters remaining.');
